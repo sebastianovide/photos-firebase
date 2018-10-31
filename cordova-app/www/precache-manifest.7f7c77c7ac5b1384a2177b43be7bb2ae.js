cc9815bf13d070d67a2e979377d5d464af86244a
Sebastian <sebastian.ovide@gmail.com>
2018-10-31 09:09:11 +0000

    added firebase

---
+++ cordova-app/www/precache-manifest.7f7c77c7ac5b1384a2177b43be7bb2ae.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "057cb0ac15b09e22ad8d",
+    "url": "/static/css/main.ac0fa2be.chunk.css"
+  },
+  {
+    "revision": "057cb0ac15b09e22ad8d",
+    "url": "/static/js/main.057cb0ac.chunk.js"
+  },
+  {
+    "revision": "6645329227aa8458de93",
+    "url": "/static/css/1.35c3df59.chunk.css"
+  },
+  {
+    "revision": "6645329227aa8458de93",
+    "url": "/static/js/1.66453292.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6468634d7642b247f4a6247b94d05553",
+    "url": "/static/media/left-arrow.6468634d.svg"
+  },
+  {
+    "revision": "2435d25813df812f3f53a7b8475f7145",
+    "url": "/static/media/logo.2435d258.svg"
+  },
+  {
+    "revision": "8659515094d85059439811bbedcbbc39",
+    "url": "/static/media/menu.86595150.svg"
+  },
+  {
+    "revision": "53853ae5f2b168b627bf208afd65b5a8",
+    "url": "/static/media/camera.53853ae5.svg"
+  },
+  {
+    "revision": "cd586af5f2464c0b1dbb32ef1b02f5be",
+    "url": "/static/media/map.cd586af5.svg"
+  },
+  {
+    "revision": "a677bc92577576813b218939eecf63f6",
+    "url": "/index.html"
+  }
+];
---
+++ cordova-app/www/precache-manifest.c4f0e97cc0a93d2ef5f9c2393f60167c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "cf6e95cbcbc35f3641ee",
+    "url": "/static/css/main.473a2d88.chunk.css"
+  },
+  {
+    "revision": "cf6e95cbcbc35f3641ee",
+    "url": "/static/js/main.cf6e95cb.chunk.js"
+  },
+  {
+    "revision": "184546e3adc71a5af637",
+    "url": "/static/css/1.d576f16d.chunk.css"
+  },
+  {
+    "revision": "184546e3adc71a5af637",
+    "url": "/static/js/1.184546e3.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6468634d7642b247f4a6247b94d05553",
+    "url": "/static/media/left-arrow.6468634d.svg"
+  },
+  {
+    "revision": "2435d25813df812f3f53a7b8475f7145",
+    "url": "/static/media/logo.2435d258.svg"
+  },
+  {
+    "revision": "8659515094d85059439811bbedcbbc39",
+    "url": "/static/media/menu.86595150.svg"
+  },
+  {
+    "revision": "53853ae5f2b168b627bf208afd65b5a8",
+    "url": "/static/media/camera.53853ae5.svg"
+  },
+  {
+    "revision": "cd586af5f2464c0b1dbb32ef1b02f5be",
+    "url": "/static/media/map.cd586af5.svg"
+  },
+  {
+    "revision": "21a43e23b33e2b70457035b6f9584047",
+    "url": "/index.html"
+  }
+];
